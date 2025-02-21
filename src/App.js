86ccbaa51a0adb990db43da97cae34646c974cb9
abineshkumar <abi1205.ak@gmail.com>
2025-02-21 17:55:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [bgColor, setBgColor] = useState("#000000");
+
+  const changeColor = () => {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setBgColor(randomColor);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body" style={{ backgroundColor: bgColor }}>
+      <button className="button" onClick={changeColor}  style={{ width: "200px", height: "40px", fontSize: "15px" }}>
+        Random Colour
+      </button>
     </div>
   );
 }
 
 export default App;
+
